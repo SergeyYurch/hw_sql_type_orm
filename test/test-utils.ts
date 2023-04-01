314fcabf2c0f81339681e620d0de3e07ed3ac5c6
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-01 20:04:46 +0300

    refactor tests

---
+++ test/test-utils.ts
@@ -0,0 +1,38 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import { useContainer } from 'class-validator';
+import {
+  BadRequestException,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from '../src/common/exception-filters/http-exception.filter';
+
+export const getApp = async () => {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
+
+  const app: INestApplication = moduleFixture.createNestApplication();
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      stopAtFirstError: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const errorsForResponse = [];
+        for (const e of errors) {
+          const key = Object.keys(e.constraints)[0];
+          errorsForResponse.push({
+            message: e.constraints[key],
+            field: e.property,
+          });
+        }
+        throw new BadRequestException(errorsForResponse);
+      },
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  await app.init();
+  return app;
+};
