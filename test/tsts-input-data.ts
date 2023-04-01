9118b911bddc00b01b92c02bd20a12891a79a55d
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-01 20:04:51 +0300

    refactor tests

---
+++ test/tsts-input-data.ts
@@ -0,0 +1,89 @@
+export const user1 = {
+  login: 'user1',
+  password: 'password1',
+  email: 'email1@gmail.com',
+};
+export const user2 = {
+  login: 'user2',
+  password: 'password2',
+  email: 'email2@gmail.com',
+};
+export const user3 = {
+  login: 'user3',
+  password: 'password3',
+  email: 'email3@gmail.com',
+};
+
+export const user4 = {
+  login: 'user4',
+  password: 'password4',
+  email: 'email4@gmail.com',
+};
+export const user5 = {
+  login: 'user5',
+  password: 'password5',
+  email: 'email5@gmail.com',
+};
+export const user6 = {
+  login: 'user6',
+  password: 'password6',
+  email: 'email6@gmail.com',
+};
+export const user7 = {
+  login: 'user7',
+  password: 'password7',
+  email: 'email7@gmail.com',
+};
+export const user8 = {
+  login: 'user8',
+  password: 'password8',
+  email: 'email8@gmail.com',
+};
+export const user9 = {
+  login: 'user9',
+  password: 'password9',
+  email: 'email9@gmail.com',
+};
+export const user10 = {
+  login: 'user10',
+  password: 'password10',
+  email: 'email10@gmail.com',
+};
+
+export const user11 = {
+  login: 'user11',
+  password: 'password11',
+  email: 'email11@gmail.com',
+};
+
+export const user12 = {
+  login: 'user12',
+  password: 'password12',
+  email: 'email12@gmail.com',
+};
+
+export const bannedUser = {
+  login: 'user3',
+  password: 'password3',
+  email: 'email3@gmail.com',
+};
+export const blog1 = {
+  name: 'blog1',
+  description: 'description1',
+  websiteUrl: 'https://youtube1.com',
+};
+export const blog2 = {
+  name: 'blog2',
+  description: 'description2',
+  websiteUrl: 'https://youtube2.com',
+};
+export const blog3 = {
+  name: 'blog3',
+  description: 'description3',
+  websiteUrl: 'https://youtube3.com',
+};
+export const blog4 = {
+  name: 'blog4',
+  description: 'description4',
+  websiteUrl: 'https://youtube4.com',
+};
