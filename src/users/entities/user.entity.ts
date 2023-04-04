ff5cfc7269f6a9e3e5674a8b4ad9ca8cbf18b5b2
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-04 11:34:30 +0300

    create user method implementation

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,21 +1,17 @@
-import { Entity, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { AccountDataEntity } from './account-data.entity';
-import { EmailConfirmationEntity } from './email-confirmation.entity';
-import { PasswordRecoveryInformationEntity } from './password-recovery-information.entity';
-import { DeviceSessionsEntity } from './device-sessions.entity';
-import { BanInfoEntity } from './ban-info.entity';
-@Entity()
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
 export class UserEntity {
   @PrimaryGeneratedColumn()
   id: string;
-  @OneToOne(() => AccountDataEntity, (ad) => ad.user)
-  accountData: AccountDataEntity;
-  @OneToOne(() => EmailConfirmationEntity, (ec) => ec.user)
-  emailConfirmation: EmailConfirmationEntity;
-  @OneToOne(() => PasswordRecoveryInformationEntity, (pri) => pri.user)
-  passwordRecoveryInformation: null | PasswordRecoveryInformationEntity;
-  @OneToMany(() => DeviceSessionsEntity, (ds) => ds.user)
-  deviceSessions: DeviceSessionsEntity[];
-  @OneToOne(() => BanInfoEntity, (bi) => bi.user)
-  banInfo: null | BanInfoEntity;
+  @Column()
+  login: string;
+  @Column()
+  email: string;
+  @Column()
+  passwordHash: string;
+  @Column()
+  passwordSalt: string;
+  @Column({ type: 'bigint' })
+  createdAt: number;
 }
