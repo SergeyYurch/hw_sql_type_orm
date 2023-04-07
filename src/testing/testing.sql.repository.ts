08dfc7aab79ac3535b5857fd0df77e5ff9b98f3c
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-07 13:12:19 +0300

    implements functional sa/users

--- src/testing/testing.sql.repository.ts
+++ src/testing/testing.sql.repository.ts
@@ -6,20 +6,21 @@ export class TestingSqlRepository {
 
   async dataBaseClear(): Promise<boolean> {
     console.log('dataBaseClear');
-    await this.dataSource.query(
-      `
-      DELETE FROM password_recovery_information;
-      DELETE FROM email_confirmation;
-      DELETE FROM device_sessions;
-      DELETE FROM ban_info;
-      DELETE FROM blogs_banned_users;
-      DELETE FROM likes;
-      DELETE FROM comments;
-      DELETE FROM posts;
-      DELETE FROM blogs;
-      DELETE FROM users;
-    `,
-    );
+    await this.dataSource.query(`
+    CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS $$
+    DECLARE
+    statements CURSOR FOR
+        SELECT tablename FROM pg_tables
+        WHERE tableowner = username AND schemaname = 'public';
+    BEGIN
+    FOR stmt IN statements LOOP
+        EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
+    END LOOP;
+    END;
+    $$ LANGUAGE plpgsql;
+
+    SELECT truncate_tables ('postgres')
+    `);
 
     return true;
   }
