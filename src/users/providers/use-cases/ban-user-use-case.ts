f4d7c5812d78cdffe9163e781e63b75e5af575ea
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-07 13:12:06 +0300

    implements functional sa/users

--- src/users/providers/use-cases/ban-user-use-case.ts
+++ src/users/providers/use-cases/ban-user-use-case.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { CommandBus, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { BanUserInputModel } from '../../dto/input-models/ban -user-input-model.dto';
-import { BanCommentCommand } from '../../../comments/providers/use-cases/ban-comment-use-case';
-import { BanCommentLikesCommand } from '../../../comments/providers/use-cases/ban-comment-likes-use-case';
-import { BanPostLikesCommand } from '../../../posts/providers/use-cases/ban-post-likes-use-case';
-import { BanPostsCommand } from '../../../posts/providers/use-cases/ban-posts-use-case';
-import { UsersSqlRepository } from '../users.sql.repository';
+import { UsersTypeOrmRepository } from '../users.typeorm.repository';
 
 export class BanUserCommand {
   constructor(
@@ -17,18 +13,11 @@ export class BanUserCommand {
 @Injectable()
 @CommandHandler(BanUserCommand)
 export class BanUserUseCase implements ICommandHandler<BanUserCommand> {
-  constructor(
-    private readonly usersRepository: UsersSqlRepository,
-    private commandBus: CommandBus,
-  ) {}
+  constructor(private readonly usersRepository: UsersTypeOrmRepository) {}
   async execute(command: BanUserCommand) {
     console.log('BanUserCommand');
     const { userId, banUserInputModel } = command;
     const { isBanned, banReason } = banUserInputModel;
-    // await this.commandBus.execute(new BanCommentCommand(userId, isBanned));
-    // await this.commandBus.execute(new BanCommentLikesCommand(userId, isBanned));
-    // await this.commandBus.execute(new BanPostsCommand({ userId, isBanned }));
-    // await this.commandBus.execute(new BanPostLikesCommand(userId, isBanned));
     const userModel = await this.usersRepository.getUserModel(userId);
     await userModel.ban(isBanned, banReason, 'saId');
     return await this.usersRepository.save(userModel);
