eb3c16642d2a89675f0b925f3ccc8c6bbef39b8e
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-09 15:57:49 +0300

    refactor dohera typeOrm-hren polnaya

--- src/testing/testing.sql.repository.ts
+++ src/testing/testing.sql.repository.ts
@@ -6,22 +6,29 @@ export class TestingSqlRepository {
 
   async dataBaseClear(): Promise<boolean> {
     console.log('dataBaseClear');
-    await this.dataSource.query(`
-    CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS $$
-    DECLARE
-    statements CURSOR FOR
-        SELECT tablename FROM pg_tables
-        WHERE tableowner = username AND schemaname = 'public';
-    BEGIN
-    FOR stmt IN statements LOOP
-        EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
-    END LOOP;
-    END;
-    $$ LANGUAGE plpgsql;
-
-    SELECT truncate_tables ('postgres')
-    `);
-
+    await this.dataSource.query(
+      `
+      DELETE FROM password_recovery_information;
+      DELETE FROM device_sessions;
+      DELETE FROM users;
+    `,
+    );
+    // const result = await this.dataSource.query(`
+    // CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS $$
+    // DECLARE
+    // statements CURSOR FOR
+    //     SELECT tablename FROM pg_tables
+    //     WHERE tableowner = username AND schemaname = 'public';
+    // BEGIN
+    // FOR stmt IN statements LOOP
+    //     EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
+    // END LOOP;
+    // END;
+    // $$ LANGUAGE plpgsql;
+    //
+    // SELECT truncate_tables ('postgres')
+    // `);
+    // console.log(result);
     return true;
   }
 }
