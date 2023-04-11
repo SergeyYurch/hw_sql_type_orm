fba64ddbe30822eeca278cd219d011fe5d3cdb1e
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-11 20:50:51 +0300

    refactor users.repositories

--- src/auth/providers/use-cases/logout-use-case.ts
+++ src/auth/providers/use-cases/logout-use-case.ts
@@ -9,9 +9,15 @@ export class LogoutUseCase implements ICommandHandler<LogoutCommand> {
   constructor(private userRepository: UsersTypeOrmRepository) {}
 
   async execute(command: LogoutCommand) {
-    const { userId, deviceId } = command;
-    const user = await this.userRepository.getUserModel(userId);
-    user.logout(deviceId);
-    return !!(await this.userRepository.save(user));
+    try {
+      const { userId, deviceId } = command;
+      const user = await this.userRepository.getUserModel(userId);
+      user.logout(deviceId);
+      await this.userRepository.save(user);
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
   }
 }
