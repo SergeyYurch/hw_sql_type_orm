051d53c8b3dcedbe59c1c8d3ff75c141b05248ad
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-11 20:50:05 +0300

    refactor users.repositories

--- src/blogs/constants/blogs.constant.ts
+++ src/blogs/constants/blogs.constant.ts
@@ -1,4 +1,4 @@
-export const WRONG_BLOG_ID = 'Blog ID is invalid';
+export const WRONG_BLOG_ID = 'BlogEntity ID is invalid';
 export const WRONG_USER_ID = 'User ID is invalid';
-export const BLOG_NOT_CREATED = 'Blog has not been created';
+export const BLOG_NOT_CREATED = 'BlogEntity has not been created';
 export const ERR_SAVE_TO_DB = 'Error saving data to DB';
