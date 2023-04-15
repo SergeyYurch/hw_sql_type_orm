57e073c384cf59a3cae088a4de982c40626c8170
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-15 13:33:07 +0300

    refactor likes

--- src/posts/providers/use-cases/update-post-like-status-use-case.ts
+++ src/posts/providers/use-cases/update-post-like-status-use-case.ts
@@ -1,6 +1,5 @@
 import { LikeStatusType } from '../../../common/dto/input-models/like.input.model';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { LikeDto } from '../../../common/dto/like.dto';
 import { UsersQueryTypeormRepository } from '../../../users/providers/users.query-typeorm.repository';
 import { PostsTypeOrmRepository } from '../posts.type-orm.repository';
 import { PostsQueryTypeOrmRepository } from '../posts.query.type-orm.repository';
@@ -30,12 +29,10 @@ export class UpdatePostLikeStatusUseCase
     );
     const user = await this.usersQueryRepository.getUserModelById(userId);
     if (!user) return null;
-    const likeDto: LikeDto = {
-      userId: user.id,
-      login: user.accountData.login,
+    postModel.updatedLike = {
+      user: user,
       likeStatus,
     };
-    postModel.updateLikeStatus(likeDto);
     return !!(await this.postRepository.save(postModel));
   }
 }
