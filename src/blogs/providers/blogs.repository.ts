4cb8dabe54a58ec1c57526b7d7dd1bfefd7cc00e
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-15 08:07:00 +0300

    refactor project

--- src/blogs/providers/blogs.repository.ts
+++ src/blogs/providers/blogs.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Blog, BlogDocument } from '../domain/blog.schema';
+import { Blog, BlogDocument } from '../mongo-shema/blog.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 
