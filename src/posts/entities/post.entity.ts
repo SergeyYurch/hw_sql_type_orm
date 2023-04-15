c9da8139b5f011e0db73c809cd8c0376a9c9746a
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-15 08:05:21 +0300

    add new entities

---
+++ src/posts/entities/post.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { UserEntity } from '../../users/entities/user.entity';
+import { BlogEntity } from '../../blogs/entities/blog.entity';
+
+@Entity('posts')
+export class PostEntity {
+  @PrimaryGeneratedColumn()
+  id: string;
+  @Column()
+  title: string;
+  @Column()
+  shortDescription: string;
+  @Column()
+  content: string;
+  @Column({ default: false })
+  isBanned: boolean;
+  @Column({ type: 'bigint' })
+  createdAt: number;
+  @ManyToOne(() => UserEntity)
+  blogger: UserEntity;
+  @ManyToOne(() => BlogEntity)
+  blog: BlogEntity;
+}
