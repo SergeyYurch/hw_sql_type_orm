d5cc7d024781ff6fb5e4dcae4be09c8089d15182
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-15 08:09:19 +0300

    refactor project

--- src/common/guards/user-blogger-ban.guard.ts
+++ src/common/guards/user-blogger-ban.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { PostsQuerySqlRepository } from '../../posts/providers/posts.query.sql.repository';
-import { BlogsQuerySqlRepository } from '../../blogs/providers/blogs.query.sql.repository';
+import { BlogsQueryTypeOrmRepository } from '../../blogs/providers/blogs.query.type-orm.repository';
 
 @Injectable()
 export class UserBloggerBanGuard implements CanActivate {
   constructor(
     private postsQueryRepository: PostsQuerySqlRepository,
-    private blogsQueryRepository: BlogsQuerySqlRepository,
+    private blogsQueryRepository: BlogsQueryTypeOrmRepository,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
