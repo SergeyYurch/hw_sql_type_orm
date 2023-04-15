f601c39e324f347c11bd07f3dfad1ecd71ff358f
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-15 08:05:12 +0300

    add new entities

---
+++ src/comments/entities/comment.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { PostEntity } from '../../posts/entities/post.entity';
+import { UserEntity } from '../../users/entities/user.entity';
+
+@Entity('comments')
+export class CommentEntity {
+  @PrimaryGeneratedColumn()
+  id: string;
+  @Column()
+  content: string;
+  @Column({ type: 'bigint' })
+  createdAt: number;
+  @Column({ type: 'bigint' })
+  updatedAt: number;
+  @ManyToOne(() => PostEntity)
+  post: PostEntity;
+  @ManyToOne(() => UserEntity)
+  commentatorId: UserEntity;
+}
