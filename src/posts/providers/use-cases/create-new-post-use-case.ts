9c35fa0a13e5e5d79fdfe3e5ef4eb318dfb3bb7c
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-15 12:11:59 +0300

    refactor postModel

--- src/posts/providers/use-cases/create-new-post-use-case.ts
+++ src/posts/providers/use-cases/create-new-post-use-case.ts
@@ -1,11 +1,9 @@
-import { PostViewModel } from '../../dto/view-models/post.view.model';
 import { CommandHandler } from '@nestjs/cqrs';
 import { PostCreateDto } from '../../dto/post-create.dto';
 import { BlogPostInputModel } from '../../../blogs/dto/input-models/blog-post.input.model';
-import { BlogsQuerySqlRepository } from '../../../blogs/providers/blogs.query.sql.repository';
-import { PostsSqlRepository } from '../posts.sql.repository';
 import { PostsTypeOrmRepository } from '../posts.type-orm.repository';
 import { BlogsQueryTypeOrmRepository } from '../../../blogs/providers/blogs.query.type-orm.repository';
+import { UsersQueryTypeormRepository } from '../../../users/providers/users.query-typeorm.repository';
 
 export class CreateNewPostCommand {
   constructor(
@@ -20,20 +18,23 @@ export class CreateNewPostUseCase {
   constructor(
     private postRepository: PostsTypeOrmRepository,
     private blogQueryRepository: BlogsQueryTypeOrmRepository,
+    private usersQueryTypeormRepository: UsersQueryTypeormRepository,
   ) {}
 
   async execute(command: CreateNewPostCommand) {
     const { userId, blogId, postInputModel } = command;
     const { shortDescription, content, title } = postInputModel;
     const createdPost = await this.postRepository.createModel();
-    const blog = await this.blogQueryRepository.getBlogById(blogId);
+    const blog = await this.blogQueryRepository.getBlogModelById(blogId);
+    const blogger = await this.usersQueryTypeormRepository.getUserModelById(
+      userId,
+    );
     const postDto: PostCreateDto = {
       title,
       shortDescription,
       content,
-      blogId,
-      bloggerId: userId,
-      blogName: blog.name,
+      blog,
+      blogger,
     };
     await createdPost.initial(postDto);
     return await this.postRepository.save(createdPost);
