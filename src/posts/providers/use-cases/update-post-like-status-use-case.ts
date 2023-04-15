37fe1145f3ac848b679d13c2c5062eb6f1d5c061
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-15 12:13:13 +0300

    refactor postModel

--- src/posts/providers/use-cases/update-post-like-status-use-case.ts
+++ src/posts/providers/use-cases/update-post-like-status-use-case.ts
@@ -1,8 +1,9 @@
 import { LikeStatusType } from '../../../common/dto/input-models/like.input.model';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { UsersQuerySqlRepository } from '../../../users/providers/users.query-sql.repository';
-import { PostsSqlRepository } from '../posts.sql.repository';
 import { LikeDto } from '../../../common/dto/like.dto';
+import { UsersQueryTypeormRepository } from '../../../users/providers/users.query-typeorm.repository';
+import { PostsTypeOrmRepository } from '../posts.type-orm.repository';
+import { PostsQueryTypeOrmRepository } from '../posts.query.type-orm.repository';
 export class UpdatePostLikeStatusCommand {
   constructor(
     public postId: string,
@@ -16,17 +17,18 @@ export class UpdatePostLikeStatusUseCase
   implements ICommandHandler<UpdatePostLikeStatusCommand>
 {
   constructor(
-    private usersQueryRepository: UsersQuerySqlRepository,
-    private postRepository: PostsSqlRepository,
+    private usersQueryRepository: UsersQueryTypeormRepository,
+    private postRepository: PostsTypeOrmRepository,
+    private postsQueryTypeOrmRepository: PostsQueryTypeOrmRepository,
   ) {}
 
   async execute(command: UpdatePostLikeStatusCommand) {
     const { postId, userId, likeStatus } = command;
-    const postModel = await this.postRepository.getPostModelById(
+    const postModel = await this.postsQueryTypeOrmRepository.getPostModelById(
       postId,
       userId,
     );
-    const user = await this.usersQueryRepository.getUserModel(userId);
+    const user = await this.usersQueryRepository.getUserModelById(userId);
     if (!user) return null;
     const likeDto: LikeDto = {
       userId: user.id,
