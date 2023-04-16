317ed80451a69da490760bdf4937670db7e5b89c
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-16 15:25:45 +0300

    implementing get/posts

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,6 +1,15 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  VirtualColumn,
+} from 'typeorm';
 import { UserEntity } from '../../users/entities/user.entity';
 import { BlogEntity } from '../../blogs/entities/blog.entity';
+import { LikeEntity } from '../../likes/entities/like.entity';
 
 @Entity('posts')
 export class PostEntity {
@@ -22,4 +31,19 @@ export class PostEntity {
   blog: BlogEntity;
   @Column()
   blogId: number;
+  @OneToMany(() => LikeEntity, (l) => l.post)
+  @JoinColumn()
+  newestLikes: LikeEntity[];
+  @VirtualColumn({
+    type: 'int',
+    query: (PostEntity) =>
+      `SELECT COUNT(*) FROM likes l LEFT JOIN users u ON u.id=l."userId"  WHERE l."postId" = ${PostEntity}.id  AND l."likeStatus"='Like' AND u."isBanned"=false`,
+  })
+  likesCount: number;
+  @VirtualColumn({
+    type: 'int',
+    query: (PostEntity) =>
+      `SELECT COUNT(*) FROM likes l LEFT JOIN users u ON u.id=l."userId"  WHERE l."postId" = ${PostEntity}.id  AND l."likeStatus"='Dislike' AND u."isBanned"=false`,
+  })
+  dislikesCount: number;
 }
