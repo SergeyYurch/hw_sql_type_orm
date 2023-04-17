90971b9d174adb327ebf912d40e39e1d038fc706
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-17 14:36:50 +0300

    global refactor

--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,11 +1,17 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  VirtualColumn,
+} from 'typeorm';
 import { PostEntity } from '../../posts/entities/post.entity';
 import { UserEntity } from '../../users/entities/user.entity';
 
 @Entity('comments')
 export class CommentEntity {
   @PrimaryGeneratedColumn()
-  id: string;
+  id: number;
   @Column()
   content: string;
   @Column({ type: 'bigint' })
@@ -14,6 +20,22 @@ export class CommentEntity {
   updatedAt: number;
   @ManyToOne(() => PostEntity)
   post: PostEntity;
+  @Column()
+  postId: number;
   @ManyToOne(() => UserEntity)
   commentator: UserEntity;
+  @Column()
+  commentatorId: number;
+  @VirtualColumn({
+    type: 'int',
+    query: (CommentEntity) =>
+      `SELECT COUNT(*) FROM likes l LEFT JOIN users u ON u.id=l."userId"  WHERE l."commentId" = ${CommentEntity}.id  AND l."likeStatus"='Like' AND u."isBanned"=false`,
+  })
+  likesCount: number;
+  @VirtualColumn({
+    type: 'int',
+    query: (CommentEntity) =>
+      `SELECT COUNT(*) FROM likes l LEFT JOIN users u ON u.id=l."userId"  WHERE l."commentId" = ${CommentEntity}.id  AND l."likeStatus"='Dislike' AND u."isBanned"=false`,
+  })
+  dislikesCount: number;
 }
