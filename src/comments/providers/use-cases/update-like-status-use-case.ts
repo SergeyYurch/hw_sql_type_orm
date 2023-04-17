3ef7c4baf4150e392e74f0044b0d9651e5fcf8f5
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-17 14:39:38 +0300

    global refactor

--- src/comments/providers/use-cases/update-like-status-use-case.ts
+++ src/comments/providers/use-cases/update-like-status-use-case.ts
@@ -1,8 +1,8 @@
 import { LikeStatusType } from '../../../common/dto/input-models/like.input.model';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { UsersQuerySqlRepository } from '../../../users/providers/users.query-sql.repository';
-import { CommentsSqlRepository } from '../comments.sql.repository';
-import { LikeDto } from '../../../likes/dto/like.dto';
+import { CommentsTypeOrmRepository } from '../comments.type-orm.repository';
+import { UsersQueryTypeormRepository } from '../../../users/providers/users.query-typeorm.repository';
+import { CommentsQueryTypeOrmRepository } from '../comments.query.type-orm.repository';
 
 export class UpdateLikeStatusCommand {
   constructor(
@@ -17,22 +17,18 @@ export class UpdateLikeStatusUseCase
   implements ICommandHandler<UpdateLikeStatusCommand>
 {
   constructor(
-    protected usersQueryRepository: UsersQuerySqlRepository,
-    protected commentsRepository: CommentsSqlRepository,
+    protected usersQueryRepository: UsersQueryTypeormRepository,
+    protected commentsRepository: CommentsTypeOrmRepository,
+    protected commentsQueryRepository: CommentsQueryTypeOrmRepository,
   ) {}
 
   async execute(command: UpdateLikeStatusCommand) {
     const { userId, likeStatus, commentId } = command;
-    const commentModel = await this.commentsRepository.getCommentModelById(
+    const commentModel = await this.commentsQueryRepository.getCommentModelById(
       commentId,
     );
-    const user = await this.usersQueryRepository.getUserById(userId);
-    const like: LikeDto = {
-      likeStatus,
-      userId,
-      login: user.login,
-    };
-    commentModel.updateLikeStatus(like);
+    const user = await this.usersQueryRepository.getUserModelById(userId);
+    commentModel.updateLikeStatus(likeStatus, user);
     await this.commentsRepository.save(commentModel);
   }
 }
