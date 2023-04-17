7c51decd3cb5ad1cbe091aca8062b36cc66aac15
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-17 14:34:13 +0300

    global refactor

--- src/blogs/providers/use-cases/ban-blog-use-case.ts
+++ src/blogs/providers/use-cases/ban-blog-use-case.ts
@@ -1,6 +1,7 @@
 import { CommandBus, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { BanPostsCommand } from '../../../posts/providers/use-cases/ban-posts-use-case';
 import { BlogsTypeOrmRepository } from '../blogs.type-orm.repository';
+import { BlogsQueryTypeOrmRepository } from '../blogs.query.type-orm.repository';
 
 export class BanBlogCommand {
   constructor(public blogId: string, public isBanned: boolean) {}
@@ -10,13 +11,17 @@ export class BanBlogCommand {
 export class BanBlogUseCase implements ICommandHandler<BanBlogCommand> {
   constructor(
     private readonly blogRepository: BlogsTypeOrmRepository,
+    private readonly blogsQueryTypeOrmRepository: BlogsQueryTypeOrmRepository,
     private commandBus: CommandBus,
   ) {}
 
   async execute(command: BanBlogCommand) {
     const { blogId, isBanned } = command;
     await this.commandBus.execute(new BanPostsCommand({ isBanned, blogId }));
-    const bannedBlog = await this.blogRepository.getBlogModel(blogId);
+    const bannedBlog = await this.blogsQueryTypeOrmRepository.getBlogModelById(
+      blogId,
+      { bannedBlogInclude: true },
+    );
     bannedBlog.banBlog(isBanned);
     return !!(await this.blogRepository.save(bannedBlog));
   }
