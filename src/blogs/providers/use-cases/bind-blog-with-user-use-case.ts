50df6af9e80ffa5ed9f9c9419664443a0fea3fc2
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-17 14:34:18 +0300

    global refactor

--- src/blogs/providers/use-cases/bind-blog-with-user-use-case.ts
+++ src/blogs/providers/use-cases/bind-blog-with-user-use-case.ts
@@ -1,6 +1,7 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { BlogsTypeOrmRepository } from '../blogs.type-orm.repository';
 import { UsersQueryTypeormRepository } from '../../../users/providers/users.query-typeorm.repository';
+import { BlogsQueryTypeOrmRepository } from '../blogs.query.type-orm.repository';
 
 export class BindBlogWithUserCommand {
   constructor(public blogId: string, public userId: string) {}
@@ -12,13 +13,16 @@ export class BindBlogWithUserUseCase
 {
   constructor(
     private blogRepository: BlogsTypeOrmRepository,
+    private blogsQueryTypeOrmRepository: BlogsQueryTypeOrmRepository,
     private userQueryRepository: UsersQueryTypeormRepository,
   ) {}
 
   async execute(command: BindBlogWithUserCommand) {
     const { userId, blogId } = command;
     const user = await this.userQueryRepository.getUserModelById(userId);
-    const editBlog = await this.blogRepository.getBlogModel(blogId);
+    const editBlog = await this.blogsQueryTypeOrmRepository.getBlogModelById(
+      blogId,
+    );
     editBlog.bindUser(userId, user.accountData.login);
     return !!(await this.blogRepository.save(editBlog));
   }
