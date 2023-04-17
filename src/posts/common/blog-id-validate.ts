6feafe86e04709b5af8ef2c90cee25413b94d865
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-17 14:45:23 +0300

    global refactor

--- src/posts/common/blog-id-validate.ts
+++ src/posts/common/blog-id-validate.ts
@@ -5,13 +5,13 @@ import {
   ValidatorConstraintInterface,
 } from 'class-validator';
 import { Injectable } from '@nestjs/common';
-import { BlogsQuerySqlRepository } from '../../blogs/providers/blogs.query.sql.repository';
+import { BlogsQueryTypeOrmRepository } from '../../blogs/providers/blogs.query.type-orm.repository';
 
 @ValidatorConstraint({ name: 'blogId', async: true })
 @Injectable()
 export class IsBlogExistConstraint implements ValidatorConstraintInterface {
   constructor(
-    protected readonly blogsQueryRepository: BlogsQuerySqlRepository,
+    protected readonly blogsQueryRepository: BlogsQueryTypeOrmRepository,
   ) {}
   async validate(blogId: string) {
     return await this.blogsQueryRepository.doesBlogIdExist(blogId);
