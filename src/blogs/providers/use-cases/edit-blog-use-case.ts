a451e62e65b6036080cc7528c22fe038dc67f0c0
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-17 14:37:57 +0300

    global refactor

--- src/blogs/providers/use-cases/edit-blog-use-case.ts
+++ src/blogs/providers/use-cases/edit-blog-use-case.ts
@@ -2,6 +2,7 @@ import { BlogInputModel } from '../../dto/input-models/blog.input.model';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { BlogEditDto } from '../../dto/blog-edit.dto';
 import { BlogsTypeOrmRepository } from '../blogs.type-orm.repository';
+import { BlogsQueryTypeOrmRepository } from '../blogs.query.type-orm.repository';
 
 export class EditBlogCommand {
   constructor(public blogId: string, public inputDate: BlogInputModel) {}
@@ -9,7 +10,10 @@ export class EditBlogCommand {
 
 @CommandHandler(EditBlogCommand)
 export class EditBlogUseCase implements ICommandHandler<EditBlogCommand> {
-  constructor(private readonly blogRepository: BlogsTypeOrmRepository) {}
+  constructor(
+    private readonly blogRepository: BlogsTypeOrmRepository,
+    private readonly blogsQueryTypeOrmRepository: BlogsQueryTypeOrmRepository,
+  ) {}
 
   async execute(command: EditBlogCommand) {
     const { blogId, inputDate } = command;
@@ -18,7 +22,9 @@ export class EditBlogUseCase implements ICommandHandler<EditBlogCommand> {
       websiteUrl: inputDate.websiteUrl,
       description: inputDate.description,
     };
-    const editBlog = await this.blogRepository.getBlogModel(blogId);
+    const editBlog = await this.blogsQueryTypeOrmRepository.getBlogModelById(
+      blogId,
+    );
     editBlog.blogUpdate(changes);
     return !!(await this.blogRepository.save(editBlog));
   }
