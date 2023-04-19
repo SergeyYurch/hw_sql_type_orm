7a833b1eb4bbf5cc7d894ccc695ecd61458be338
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-19 14:29:13 +0300

    add question.ts

---
+++ src/quiz/domain/question.ts
@@ -0,0 +1,21 @@
+import { CreateQuestionDto } from '../dto/create-question.dto';
+
+export class QuizQuestionEntity {
+  id: number;
+  body: string;
+  correctAnswers: string[];
+  published: boolean;
+  createdAt: number;
+  updatedAt: number;
+  constructor(createData: CreateQuestionDto) {
+    this.body = createData.body;
+    this.correctAnswers = createData.correctAnswers;
+    this.published = false;
+    this.createdAt = Date.now();
+    this.updatedAt = Date.now();
+  }
+  publishedQuestion(published: boolean) {
+    this.published = published;
+    this.updatedAt = Date.now();
+  }
+}
