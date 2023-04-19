c34444c6ba7f6b238a65b20c54c46f1fb3fb720d
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-19 14:29:44 +0300

    fix

--- src/blogs/domain/blog.ts
+++ src/blogs/domain/blog.ts
@@ -1,7 +1,6 @@
 import { BlogCreatedDto } from '../dto/blog-created.dto';
 import { BlogEditDto } from '../dto/blog-edit.dto';
 import { BlogDbDtoSql } from '../types/blog-db-dto.sql';
-import { SchemaOfChangeDetectionType } from '../../common/types/schema-of-change-detection.type';
 
 export class BannedUser {
   id: string;
@@ -35,24 +34,6 @@ export class Blog {
     this.banDate = null;
     this.bannedUsers = [];
   }
-
-  setDbData(blogDb: BlogDbDtoSql, bannedUsers: BannedUser[]) {
-    this.id = blogDb.id;
-    this.name = blogDb.name;
-    this.websiteUrl = blogDb.websiteUrl;
-    this.description = blogDb.description;
-    this.createdAt = +blogDb.createdAt;
-    this.blogOwnerId = blogDb.blogOwnerId;
-    this.blogOwnerLogin = blogDb.blogOwnerLogin;
-    this.isMembership = blogDb.isMembership;
-    this.isBanned = blogDb.isBanned;
-    this.banDate = +blogDb.banDate;
-    this.bannedUsers = [];
-    if (bannedUsers.length > 0) {
-      this.bannedUsers = bannedUsers;
-    }
-  }
-
   blogUpdate(changes: BlogEditDto) {
     this.name = changes.name;
     this.description = changes.description;
@@ -86,19 +67,3 @@ export class Blog {
     }
   }
 }
-
-//determines the correspondence of the fields of the entity to the table and the fields of the database
-export const blogSchemaDb: SchemaOfChangeDetectionType[] = [
-  {
-    tableName: 'blogs',
-    fields: [
-      { fieldName: 'name', dbFiledName: 'name' },
-      { fieldName: 'blogOwnerId', dbFiledName: 'blogOwnerId' },
-      { fieldName: 'description', dbFiledName: 'description' },
-      { fieldName: 'websiteUrl', dbFiledName: 'websiteUrl' },
-      { fieldName: 'isMembership', dbFiledName: 'isMembership' },
-      { fieldName: 'isBanned', dbFiledName: 'isBanned' },
-      { fieldName: 'banDate', dbFiledName: 'banDate' },
-    ],
-  },
-];
--- src/quiz/sa-quiz-questions.controller.ts
+++ src/quiz/sa-quiz-questions.controller.ts
@@ -1,15 +1,32 @@
-import { Controller, Delete, Get, Post, Put, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { QuestionInputModel } from './dto/inputModels/question.input.model';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateQuestionCommand } from './providers/use-cases/create-question.use-case';
 
 @UseGuards(AuthGuard('basic'))
 @Controller('sa/quiz/questions')
 export class SaQuizQuestionsController {
+  constructor(private commandBus: CommandBus) {}
   @Get()
   async getQuestions() {
     return true;
   }
+
   @Post()
-  async createQuestion() {
+  async createQuestion(@Body() question: QuestionInputModel) {
+    const questionId = this.commandBus.execute(
+      new CreateQuestionCommand(question),
+    );
     return true;
   }
 
@@ -19,11 +36,13 @@ export class SaQuizQuestionsController {
   }
 
   @Put(':id')
+  @HttpCode(204)
   async updateQuestion() {
     return true;
   }
 
   @Put(':id/publish')
+  @HttpCode(204)
   async publishQuestion() {
     return true;
   }
