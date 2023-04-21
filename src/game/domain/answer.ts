14fec3b2b2d92611c28ce89d1f5c369ae0689cae
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-21 15:59:23 +0300

    implement pair functional

---
+++ src/game/domain/answer.ts
@@ -0,0 +1,9 @@
+import { Question } from '../../quiz/domain/question';
+
+export class Answer {
+  id: string;
+  question: Question;
+  answerStatus: 'Correct' | 'Incorrect';
+  body: string;
+  addedAt: number;
+}
