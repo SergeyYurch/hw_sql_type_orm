fd558650ba19fede4d830f2364cb6348179bec49
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-04-21 16:00:47 +0300

    implement pair functional

---
+++ src/game/entities/player.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { UserEntity } from '../../users/entities/user.entity';
+import { AnswerEntity } from './ansver.entity';
+
+@Entity('players')
+export class PlayerEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  userId: number;
+  @ManyToOne(() => UserEntity)
+  user: UserEntity;
+  @OneToMany(() => AnswerEntity, (a) => a.player)
+  @JoinColumn()
+  answers: AnswerEntity[];
+  @Column()
+  score: number;
+}
