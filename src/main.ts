b37b65da90cc187e646ccc37c08ab7a82a3f501b
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-05-15 11:57:39 +0300

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { BadRequestException, ValidationPipe } from '@nestjs/common';
-import { HttpExceptionFilter } from './common/exception-filters/http-exception.filter';
-import cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { useContainer } from 'class-validator';
-import { swaggerSetup } from './config/swagger-setup';
+import { configApp } from './config/config-app';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors();
   app.set('trust proxy', 0);
   // app.setGlobalPrefix('api');
-  app.use(cookieParser());
-  useContainer(app.select(AppModule), { fallbackOnErrors: true });
-  app.useGlobalPipes(
-    new ValidationPipe({
-      stopAtFirstError: true,
-      transform: true,
-      exceptionFactory: (errors) => {
-        const errorsForResponse = [];
-        for (const e of errors) {
-          const key = Object.keys(e.constraints)[0];
-          errorsForResponse.push({
-            message: e.constraints[key],
-            field: e.property,
-          });
-        }
-        throw new BadRequestException(errorsForResponse);
-      },
-    }),
-  );
-  app.useGlobalFilters(new HttpExceptionFilter());
-  swaggerSetup(app);
+  configApp(app);
   const port = process.env.PORT;
-  console.log(port);
   await app.listen(port);
 }
 bootstrap();
