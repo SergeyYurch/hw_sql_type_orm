8de808b0b2b8bcbf0defb7a7171f3693f5e0568b
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-05-15 11:57:20 +0300

    refactor

---
+++ test/helpers/accounts.test.helpers.ts
@@ -0,0 +1,50 @@
+import { UsersTestHelpers } from './users.test.helpers';
+import { UserInputModel } from '../../src/features/users/dto/input-models/user-input-model';
+import { LoginInputModel } from '../../src/features/auth/dto/login.input.model';
+import { AuthTestHelpers } from './auth.test.helpers';
+import { INestApplication } from '@nestjs/common';
+
+export class AccountsTestHelpers {
+  usersTestService: UsersTestHelpers;
+  authTestHelpers: AuthTestHelpers;
+  constructor(private app: INestApplication) {
+    this.usersTestService = new UsersTestHelpers(app);
+    this.authTestHelpers = new AuthTestHelpers(app);
+  }
+  getUserInputModel(n: number) {
+    return {
+      login: `user${n}`,
+      password: `password${n}`,
+      email: `email${n}@gmail.com`,
+    };
+  }
+  async createSetOfUsers(count: number, startNumber = 1) {
+    for (let i = 0; i < count; i++) {
+      console.log('t12');
+      console.log(`i:${i}`);
+      console.log(`count:${count}`);
+      console.log(`startNumber:${startNumber}`);
+      const user: UserInputModel = this.getUserInputModel(startNumber);
+      console.log(`user:${user.login}`);
+      await this.usersTestService.createUser(user, 201);
+      startNumber++;
+    }
+  }
+  async createAndLoginUsers(count: number, startNumber = 1) {
+    const accessTokens = [];
+    const refreshTokens = [];
+    await this.createSetOfUsers(count, startNumber);
+    for (let i = 0; i < count; i++) {
+      const loginData: LoginInputModel = {
+        loginOrEmail: `user${startNumber}`,
+        password: `password${startNumber}`,
+      };
+      const { refreshToken, accessToken } =
+        await this.authTestHelpers.loginUser(loginData);
+      accessTokens.push(accessToken);
+      refreshTokens.push(refreshToken);
+      startNumber++;
+    }
+    return { accessTokens, refreshTokens };
+  }
+}
