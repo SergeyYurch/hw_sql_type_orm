061faf3307dd1b906ebdb5a089fb2fc1f7ead879
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2023-05-18 11:38:55 +0300

    refactor testing.type-orm.repository.ts

--- src/features/testing/testing.type-orm.repository.ts
+++ src/features/testing/testing.type-orm.repository.ts
@@ -6,40 +6,39 @@ export class TestingTypeOrmRepository {
 
   async dataBaseClear(): Promise<boolean> {
     console.log('dataBaseClear');
-    await this.dataSource.query(
-      `
-      DELETE FROM answers;
-      DELETE FROM pairs;
-      DELETE FROM players;
-      DELETE FROM quiz_questions;
-      DELETE FROM likes;
-      DELETE FROM comments;
-      DELETE FROM posts;
-      DELETE FROM blogs_banned_users;
-      DELETE FROM blogs;
-      DELETE FROM password_recovery_information;
-      DELETE FROM device_sessions;
-      DELETE FROM users;
-
-      
-    `,
-    );
-    // const result = await this.dataSource.query(`
-    // CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS $$
-    // DECLARE
-    // statements CURSOR FOR
-    //     SELECT tablename FROM pg_tables
-    //     WHERE tableowner = username AND schemaname = 'public';
-    // BEGIN
-    // FOR stmt IN statements LOOP
-    //     EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
-    // END LOOP;
-    // END;
-    // $$ LANGUAGE plpgsql;
+    // await this.dataSource.query(
+    //   `
+    //   DELETE FROM answers;
+    //   DELETE FROM pairs;
+    //   DELETE FROM players;
+    //   DELETE FROM quiz_questions;
+    //   DELETE FROM likes;
+    //   DELETE FROM comments;
+    //   DELETE FROM posts;
+    //   DELETE FROM blogs_banned_users;
+    //   DELETE FROM blogs;
+    //   DELETE FROM password_recovery_information;
+    //   DELETE FROM device_sessions;
+    //   DELETE FROM users;
     //
-    // SELECT truncate_tables ('postgres')
-    // `);
-    // console.log(result);
+    //
+    // `,
+    // );
+    await this.dataSource.query(`
+    CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS $$
+    DECLARE
+    statements CURSOR FOR
+        SELECT tablename FROM pg_tables
+        WHERE tableowner = username AND schemaname = 'public';
+    BEGIN
+    FOR stmt IN statements LOOP
+        EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
+    END LOOP;
+    END;
+    $$ LANGUAGE plpgsql;
+
+    SELECT truncate_tables ('postgres')
+    `);
     return true;
   }
 }
